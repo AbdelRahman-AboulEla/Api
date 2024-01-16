78b5b6aa2da8d3d9657c3e75779d1fd2b6a43b81
AbdelRahmanAboulEla <126163624+AbdelRahmanAboulEla@users.noreply.github.com>
2024-01-16 21:17:39 +0200

    ,..,

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const axios = require('axios');
-const { JSDOM } = require('jsdom');
+const express = require("express");
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Express.js app!');
+app.get("/", (req, res) => {
+  res.send("Welcome to my Express.js app!");
 });
-app.get('/fetchMangaChapters/:mangaTitle', async (req, res) => {
+app.get("/fetchMangaChapters/:mangaTitle", async (req, res) => {
   const mangaTitle = req.params.mangaTitle;
-  const mangaBaseUrl = 'https://manga-lek.net'; 
+  const mangaBaseUrl = "https://manga-lek.net";
   try {
     const response = await axios.get(`${mangaBaseUrl}/manga/${mangaTitle}`);
-    
+
     if (response.status === 200) {
       const dom = new JSDOM(response.data);
-      const chapterList = dom.window.document.querySelectorAll('.wp-manga-chapter');
+      const chapterList =
+        dom.window.document.querySelectorAll(".wp-manga-chapter");
       const chapters = [];
 
       chapterList.forEach((chapter) => {
         const chapterNumber = chapter.children[0].textContent.trim();
-        chapters.push(
-          chapterNumber
-        );
+        chapters.push(chapterNumber);
       });
 
       res.json(chapters);
     } else {
       console.error(`Failed to load website: ${response.status}`);
-      res.status(response.status).send('Failed to load website');
+      res.status(response.status).send("Failed to load website");
     }
   } catch (error) {
     console.error(`Error fetching chapters: ${error.message}`);
-    res.status(500).send('Error fetching chapters');
+    console.error(error);
+    res.status(500).send("Error fetching chapters");
   }
 });
 
