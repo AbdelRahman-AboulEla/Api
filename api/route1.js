ff909b5bd64793fc9875a4e2c7c8cbe5706267be
AbdelRahmanAboulEla <126163624+AbdelRahmanAboulEla@users.noreply.github.com>
2024-01-19 21:08:27 +0200

    .

--- api/route1.js
+++ api/route1.js
@@ -5,8 +5,15 @@ module.exports = async (req, res) => {
   try {
     // Your route logic here
     const response = await axios.get(
-      "https://manga-lek.net/manga/omniscient-readers/"
+      "https://manga-lek.net/manga/omniscient-readers/",
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        },
+      }
     );
+
     res.status(200).json({ data: response.data });
   } catch (error) {
     console.error(error);
