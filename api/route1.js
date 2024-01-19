ab446f07ff4087ca21ad5589ef0ce39354d2b81f
AbdelRahmanAboulEla <126163624+AbdelRahmanAboulEla@users.noreply.github.com>
2024-01-19 21:15:08 +0200

    .

--- api/route1.js
+++ api/route1.js
@@ -4,15 +4,7 @@ const axios = require("axios");
 module.exports = async (req, res) => {
   try {
     // Your route logic here
-    const response = await axios.get(
-      "https://manga-lek.net/manga/omniscient-readers/",
-      {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        },
-      }
-    );
+    const response = await axios.get("https://www.google.com/");
 
     res.status(200).json({ data: response.data });
   } catch (error) {
--- api/route2.js
+++ api/route2.js
@@ -5,8 +5,17 @@ module.exports = async (req, res) => {
   try {
     // Your route logic here
     const response = await axios.get(
-      "https://manga-lek.net/manga/kill-the-hero/"
+      "https://manga-lek.net/manga/omniscient-readers/",
+      {
+        maxRedirects: 5, // Set an appropriate number
+        timeout: 5000, // 5000 milliseconds (adjust as needed)
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        },
+      }
     );
+
     res.status(200).json({ data: response.data });
   } catch (error) {
     console.error(error);
